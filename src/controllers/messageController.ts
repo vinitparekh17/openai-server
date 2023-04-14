4fc7300bd272edb8ab1dd41e68acdb71d8748783
Vinit Parekh <vinitrevaliya@gmail.com>
2023-04-14 12:44:05 +0530

    streams handling with sockets

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,15 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { openai } from "../lib/Openai";
+import { io } from "../lib/Socket";
 import DataProvider from "../utils/Dataprovider";
 import messageSchema from "../models/Message.schema";
 import { ErrorRes, SuccessRes } from "../utils/Responders";
+import Logger from "../utils/Logger";
 
 let MessageProvider = new DataProvider(messageSchema);
 export const generateResponse = async (req: Request, res: Response): Promise<any> => {
     try {
         let { prompt } = req.body
         let { id } = req.params
-        const completion = await openai.createChatCompletion({
+        const completionPromise = openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             max_tokens: 200,
             temperature: 0.7,
@@ -18,9 +20,19 @@ export const generateResponse = async (req: Request, res: Response): Promise<any
             user: id,
             stream: true
         })
-        if (!completion) new ErrorRes(res, 400, "Unable to generate response!")
-        let answer = completion.data.choices[0].message.content
-        new SuccessRes(res, 200, answer)
+
+        io.on('connect', () => {
+            Logger.debug('connected to socket!')
+
+            completionPromise
+            .then(completion => io.emit('create_completion', completion))
+            .catch(err => Logger.error(err));
+            
+            io.on('data', data => io.emit('completion', data.choices[0].text));
+            io.on('error', e => Logger.error(e));
+            io.on('disconnect', () => Logger.debug('Disconnected from socket!'))
+        })
+
     } catch (error) {
         new ErrorRes(res, 500, "Internal server error!")
     }
