e4755c7204a90625c3f045b39f2581230560c831
Vinit Parekh <vinitrevaliya@gmail.com>
2023-05-01 20:02:39 +0530

    origin

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -36,7 +36,7 @@ export default {
       app.use(cookieParser())
       app.use(express.json());
       app.use(cors({
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "https://omnisive.technetic.co.in"],
         methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
         credentials: true
       }));
