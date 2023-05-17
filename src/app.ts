6501952bc622b45d241a40d2baae7337d28dac61
Vinit Parekh <vinitrevaliya@gmail.com>
2023-05-17 21:02:56 +0530

    path error

--- src/app.ts
+++ src/app.ts
@@ -12,11 +12,11 @@ Middleware.init();
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
-// app.use("*", (req: Request, res: Response) => {
-//   const err = Error(`Requested path: ${req.path} not found!`);
-//   res.status(404).json({
-//     success: false,
-//     message: err.message,
-//     stack: err.stack,
-//   });
-// });
+app.use("*", (req: Request, res: Response) => {
+  const err = Error(`Requested path: ${req.path} not found!`);
+  res.status(404).json({
+    success: false,
+    message: err.message,
+    stack: err.stack,
+  });
+});
