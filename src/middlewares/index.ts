f52d1960c9d2ccf6adf3a048ef0ae228a98f5a41
vinitparekh17 <vinitparekh1742@gmail.com>
2024-03-22 01:26:10 +0530

    refactor: bug fix

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -28,6 +28,8 @@ class Middlewares {
       app.use(
         cors({
           origin: ['http://localhost:3000', 'https://omnisive.technetic.co.in'],
+          methods: ['GET', 'POST', 'PUT', 'DELETE'],
+          optionsSuccessStatus: 200,
           credentials: true,
         }),
       );
